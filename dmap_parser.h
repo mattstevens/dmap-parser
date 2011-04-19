9cca1276354da53c8e72903379b2da5825bcf8db
Matt Stevens <matt@alloysoft.com>
2011-04-18 23:21:22 -0600

    I likes the alignment

--- dmap_parser.h
+++ dmap_parser.h
@@ -8,7 +8,7 @@ extern "C" {
 
 typedef void (*dmap_int32_cb) (void *ctx, const char* code, const char* name, int32_t value);
 typedef void (*dmap_int64_cb) (void *ctx, const char* code, const char* name, int64_t value);
-typedef void (*dmap_data_cb) (void *ctx, const char* code, const char* name, const char* buf, int len);
+typedef void (*dmap_data_cb)  (void *ctx, const char* code, const char* name, const char* buf, int len);
 
 typedef struct {
 	dmap_int32_cb on_int32;
@@ -19,7 +19,7 @@ typedef struct {
 } dmap_settings;
 
 const char* dmap_name_from_code(const char* code);
-	int dmap_parse(const dmap_settings* settings, const char* buf, int len);
+int dmap_parse(const dmap_settings* settings, const char* buf, int len);
 
 #ifdef __cplusplus
 }
